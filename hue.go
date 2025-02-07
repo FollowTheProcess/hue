e929d6cd19c31a6c94c30ee0a1c35e08e6ef563d
Tom Fleet <tom.fleet@chetwood.co>
2025-02-07 09:26:03 +0000

    Optimise style combinations and string building (#13)
    ## Summary
    
    <!-- Describe your changes in detail here, if it closes an open issue,
    include "Closes #<issue>" -->
    Optimised for the normal case of only a few styles by using the same
    trick as `log/slog`. Keep a stack allocated array with `n` elements,
    then any overflow spills over to a `[]string`.
    
    So the fast case (6 styles or less) is ~7% faster, this covers basically
    all normal usage of the API. Only pathological combinations exceed this.
    At the cost of the slow case (> 6 styles) being ~18% slower.
    
    This is fine as a trade off because combining more than 6 styles is
    nonsensical in terms of an actual colour, literally all normal usage
    will fit into the fast path
    
    ```plaintext
    goos: darwin
    goarch: arm64
    pkg: github.com/FollowTheProcess/hue
    cpu: Apple M1 Pro
                           │ before.txt  │              after.txt              │
                           │   sec/op    │   sec/op     vs base                │
    Style/simple-8           2.332n ± 0%   2.330n ± 0%   -0.09% (p=0.000 n=30)
    Style/composite_fast-8   71.13n ± 0%   66.17n ± 0%   -6.98% (p=0.000 n=30)
    Style/composite_slow-8   168.2n ± 0%   199.8n ± 0%  +18.79% (p=0.000 n=30)
    geomean                  30.33n        31.35n        +3.35%
    
                           │  before.txt  │              after.txt               │
                           │     B/op     │    B/op     vs base                  │
    Style/simple-8           0.000 ± 0%     0.000 ± 0%        ~ (p=1.000 n=30) ¹
    Style/composite_fast-8   16.00 ± 0%     16.00 ± 0%        ~ (p=1.000 n=30) ¹
    Style/composite_slow-8   216.0 ± 0%     160.0 ± 0%  -25.93% (p=0.000 n=30)
    geomean                             ²                -9.52%                ²
    ¹ all samples are equal
    ² summaries must be >0 to compute geomean
    
                           │  before.txt  │               after.txt               │
                           │  allocs/op   │ allocs/op   vs base                   │
    Style/simple-8           0.000 ± 0%     0.000 ± 0%         ~ (p=1.000 n=30) ¹
    Style/composite_fast-8   1.000 ± 0%     1.000 ± 0%         ~ (p=1.000 n=30) ¹
    Style/composite_slow-8   2.000 ± 0%     5.000 ± 0%  +150.00% (p=0.000 n=30)
    geomean                             ²                +35.72%                ²
    ¹ all samples are equal
    ² summaries must be >0 to compute geomean
    
    ```

--- hue.go
+++ hue.go
@@ -223,7 +223,7 @@ func (s Style) Code() (string, error) { //nolint: cyclop // switch case is signi
 	}
 
 	// Combinations
-	styles := make([]string, 0, numStyles)
+	var c codes
 
 	for style := Bold; style <= BrightWhiteBackground; style <<= 1 {
 		// If the given style has this style bit set, add its code to the string
@@ -233,11 +233,11 @@ func (s Style) Code() (string, error) { //nolint: cyclop // switch case is signi
 				return "", err
 			}
 
-			styles = append(styles, code)
+			c.add(code)
 		}
 	}
 
-	return strings.Join(styles, ";"), nil
+	return c.String(), nil
 }
 
 // Fprint formats using the default formats for its operands and writes to w.
@@ -353,4 +353,57 @@ func autoDetectEnabled() bool {
 
 	// Can't detect otherwise so be safe and disable colour
 	return false
+}
+
+type codes struct {
+	front  [numStyles]string
+	back   []string
+	nFront int
+}
+
+func (c *codes) add(str string) {
+	if c.nFront < len(c.front) {
+		// There's room in the stack buffer
+		c.front[c.nFront] = str
+		c.nFront++
+		return
+	}
+
+	// Slower, we've filled up the stack buffer so must now append
+	// to the back slice
+	c.back = append(c.back, str)
+}
+
+func (c *codes) String() string {
+	var n int
+	var b strings.Builder
+
+	// Fast path: only the stack buffer is used
+	for _, code := range c.front {
+		if code != "" {
+			n += len(code) + 1 // 1 = len(";")
+		}
+	}
+	b.Grow(n)
+
+	if c.front[0] != "" {
+		b.WriteString(c.front[0])
+	}
+	for _, code := range c.front[1:] {
+		if code != "" {
+			b.WriteByte(';')
+			b.WriteString(code)
+		}
+	}
+
+	// Slow path, more than 6 styles in combo (which is sort of nonsensical)
+	// so fall back to the slow, growable slice
+	if len(c.back) != 0 {
+		for _, code := range c.back {
+			b.WriteByte(';')
+			b.WriteString(code)
+		}
+	}
+
+	return b.String()
 }
